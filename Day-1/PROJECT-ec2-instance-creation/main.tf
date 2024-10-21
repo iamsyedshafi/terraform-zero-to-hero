057e61bcee8c3ec3eab154e080a429a8665c181d
Syed Shafiq <sshafi067@gmail.com>
2024-10-20 23:14:40 -0400

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0324a83b82023f0b3"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
